ea78c425f8015da8bb0e5869d0e55c2ec6eadc53
hannahGu <yuguichen136@yeah.net>
2025-06-16 08:37:34 +0800

    fix: copilot review

---
+++ .eslintrc.js
@@ -0,0 +1,58 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+  },
+  plugins: ['prettier', 'only-warn'],
+  extends: ['plugin:vue/essential', '@vue/airbnb'],
+  globals: {
+    uni: true,
+    getCurrentPages: 'readonly',
+    getApp: 'readonly',
+    wx: 'readonly',
+  },
+  rules: {
+    'no-underscore-dangle': 'off',
+    'no-console': 'off',
+    semi: 'off',
+    'operator-assignment': 'off',
+    'no-unused-expressions': 'off',
+    'class-methods-use-this': 'off',
+    'import/extensions': 'off',
+    'no-use-before-define': 'off',
+    'prefer-rest-params': 'off',
+    'no-restricted-properties': 'off',
+    'guard-for-in': 'off',
+    'no-param-reassign': 'off',
+    'no-restricted-syntax': 'off',
+    'no-useless-return': 'off',
+    'no-prototype-builtins': 'off',
+    'no-nested-ternary': 'off',
+    'global-require': 'off',
+    'no-mixed-operators': 'off',
+    radix: 'off',
+    'no-tabs': 'off',
+    'no-dupe-keys': 'off',
+    'max-len': [
+      'warn',
+      {
+        code: 150,
+      },
+    ],
+    'no-shadow': 'off',
+    'linebreak-style': 'off',
+    'func-names': 'off',
+    'no-plusplus': 'off',
+    'import/prefer-default-export': 'off',
+    'no-unused-vars': [
+      'warn',
+      {
+        argsIgnorePattern: '_',
+      },
+    ],
+    'prettier/prettier': 'off',
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+};
